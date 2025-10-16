5bfc99b1cb42a10c320d5ca81dcd3e832ea26e0d
ESHWAR LADE <eshwar341arjun@gmail.com>
2025-10-16 21:54:57 +0530

    backend project

---
+++ springpro/src/main/java/com/example/springpro/controllers/SampleController.java
@@ -0,0 +1,28 @@
+package com.example.springpro.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/random")
+public class SampleController {
+
+     @GetMapping("/hello/{numberOfTimes}")
+    public String sayHello(@PathVariable("numberOfTimes") int numberOfTimes){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<numberOfTimes;i++){
+            sb.append("Akshitha\n");
+        }
+        return sb.toString();
+//         return "Hello Springboot";
+    }
+
+//    public String sayBye(){
+//
+//         return "bye everyone";
+//    }
+}
+//https://www.amazon.com/search?query='iphone'
